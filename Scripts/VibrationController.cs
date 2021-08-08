67f9cb06e31ee4828f1a12499a5eb292f079de26
GuoJinMing@mbp <tdouguo@163.com>
2021-08-08 19:11:06 +0800

    震动插件

---
+++ Scripts/VibrationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+using MoreMountains.NiceVibrations;
+
+namespace Fishtail.PlayTheBall.Vibration
+{
+    public class VibrationController : MonoBehaviour
+    {
+        public static VibrationController instance { get; private set; }
+
+        public bool vibrate { get; set; }
+
+        private void Awake()
+        {
+            instance = this;
+
+            MMVibrationManager.iOSInitializeHaptics();
+        }
+
+        private void OnDestroy()
+        {
+			MMVibrationManager.iOSReleaseHaptics();
+        }
+
+        public void Impact()
+        {
+            if (!vibrate) { return; }
+            MMVibrationManager.Haptic(HapticTypes.LightImpact);
+        }
+    }
+}
